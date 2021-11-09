b72d2eb2932ebeb85ccc7839454a23cb37df80b2
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2021-11-09 12:20:17 +0100

    Create GameObject.cs

---
+++ GameObject.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sokoban
+{
+
+
+    public abstract class GameObject
+    {
+        public Vector2 position;
+        public float rotaton;
+        public Vector2 scale;
+
+        // rendering
+        public float layerDepth;
+        protected SpriteEffects effect;
+        protected Rectangle rectangle;
+        protected Texture2D[] animations;
+        protected Texture2D sprite;
+        protected float animationSpeed;
+
+        public Vector2 Origen { 
+            get {
+                if (sprite != null)
+                {
+                    return new Vector2(sprite.Width / 2, sprite.Height / 2 );
+                }
+                return Vector2.Zero;
+            } 
+        } 
+
+    }
+}
