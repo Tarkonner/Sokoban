73f2b12ebf97c4b8f7b4cd1d2a12536cdf2107f0
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2021-11-11 18:47:35 +0100

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -8,7 +8,7 @@ namespace Sokoban
     public class Player : GameObjectWithCollider
     {
         private GraphicsDeviceManager graphicsPlayer;
-        
+
         //Movement
         public Vector2 playerInput = Vector2.Zero;
         private Vector2 gridPosition;
@@ -19,6 +19,11 @@ namespace Sokoban
 
         //Animation
 
+        protected Texture2D[] animationsW;
+        protected Texture2D[] animationsA;
+        protected Texture2D[] animationsS;
+        protected Texture2D[] animationsD;
+        protected Texture2D[] animationsx;
 
         public Player(Vector2 position, GraphicsDeviceManager graphics)
         {
@@ -34,14 +39,39 @@ namespace Sokoban
         public override void LoadContent(ContentManager content)
         {
             //Load animations
-            animations = new Texture2D[3];
+            animationsW = new Texture2D[3];
+            animationsx = new Texture2D[2];
+            animationsA = new Texture2D[3];
+            animationsS = new Texture2D[2];
+            animationsD = new Texture2D[3];
 
             for (int i = 2; i <= 4; i++)
             {
-                animations[i - 2] = content.Load<Texture2D>("player_0" + i);
+                animationsW[i - 2] = content.Load<Texture2D>("player_0" + i);
+            }
+            for (int i = 11; i <= 13; i++)
+            {
+                animationsD[i - 11] = content.Load<Texture2D>("player_" + i);
+            }
+            for (int i = 14; i <= 16; i++)
+            {
+                animationsA[i - 14] = content.Load<Texture2D>("player_" + i);
+            }
+            for (int i = 23; i <= 24; i++)
+            {
+                animationsS[i - 23] = content.Load<Texture2D>("player_" + i);
+
+            }
+            for (int i = 1; i <= 2; i++)
+            {
+                animationsx[i - 1] = content.Load<Texture2D>("player_0" + i);
+
             }
 
+           // Idle animation
+            animations = animationsx;
             sprite = animations[0];
+
 
             //Rec
             rectangle = new Rectangle((int)position.X, (int)position.Y, sprite.Width, sprite.Height);
@@ -60,19 +90,28 @@ namespace Sokoban
             if (keyboard.IsKeyDown(Keys.D))
             {
                 playerInput = new Vector2(1, 0);
-                
+                animations = animationsD;
+
             }
             else if (keyboard.IsKeyDown(Keys.A))
             {
+
                 playerInput = new Vector2(-1, 0);
+                animations = animationsA;
+
             }
             else if (keyboard.IsKeyDown(Keys.W))
             {
                 playerInput = new Vector2(0, -1);
+
+                animations = animationsW;
+
             }
             else if (keyboard.IsKeyDown(Keys.S))
             {
                 playerInput = new Vector2(0, 1);
+                animations = animationsS;
+
             }
 
 
@@ -83,8 +122,8 @@ namespace Sokoban
             if (movementClock <= 0 && playerInput != Vector2.Zero && !placeTaken.Item1)
             {
                 //Bounds
-                if(gridPosition.X + playerInput.X >= 0
-                    && gridPosition.X + playerInput.X < maxX 
+                if (gridPosition.X + playerInput.X >= 0
+                    && gridPosition.X + playerInput.X < maxX
                     && gridPosition.Y + playerInput.Y >= 0
                     && gridPosition.Y + playerInput.Y < maxY
                     )
