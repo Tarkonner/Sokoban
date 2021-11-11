43e812ff05401256874036224b67257c33d28572
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2021-11-11 12:53:42 +0100

    dwada

--- Player.cs
+++ Player.cs
@@ -8,11 +8,12 @@ namespace Sokoban
     public class Player : GameObjectWithCollider
     {
         public Vector2 playerInput = Vector2.Zero;
-
+        private bool trueAnimationsW;
         private Vector2 gridPosition;
 
         private GraphicsDeviceManager graphicsPlayer;
-        private bool placeTaken = false;
+
+
 
         private float timeBetweenMovement = .3f;
         private float movementClock = 0;
@@ -26,37 +27,75 @@ namespace Sokoban
 
             this.graphicsPlayer = graphics;
 
-            animationSpeed = 2;
+            animationSpeed = 12;
         }
+
+
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, rectangle, Color.White, rotaton, Origen, scale, effect, layerDepth);
+        }
+
 
 
         public override void LoadContent(ContentManager content)
         {
 
+            //W
             animations = new Texture2D[3];
-            //animations = new Texture2D[24];
-            //animations = new Texture2D[24];
+            //D
+            animationsD = new Texture2D[3];
+            //A
+            animationsA = new Texture2D[3];
+            //S
+            animationsS = new Texture2D[3];
 
+
+
+            //D
+            for (int i = 11; i <= 13; i++)
+            {
+
+                animationsD[i - 1] = content.Load<Texture2D>("player_" + i);
+
+
+
+            }
+            //W
             for (int i = 2; i <= 4; i++)
             {
-                animations[i - 2] = content.Load<Texture2D>("player_0" + i);
+      animations[i - 2] = content.Load<Texture2D>("player_0" + i);
+                    
+              
+
+
             }
-
-
-            //animations[0] = content.Load<Texture2D>("player_01");
-
 
             sprite = animations[0];
 
-            rectangle = new Rectangle((int)position.X, (int)position.Y, sprite.Width, sprite.Height);
+            sprite = animationsD[0];
+
+
+
+
+
+
+
+            rectangle = new Rectangle(0, 0, sprite.Width, sprite.Height);
+
+
+
+
+
         }
 
 
         private void Movement(GameTime gameTime)
         {
             playerInput = Vector2.Zero;
-            KeyboardState keyboard = Keyboard.GetState();
 
+            KeyboardState keyboard = Keyboard.GetState();
             float de = (float)gameTime.ElapsedGameTime.TotalSeconds;
             movementClock -= de;
 
@@ -64,7 +103,7 @@ namespace Sokoban
             if (keyboard.IsKeyDown(Keys.D))
             {
                 playerInput = new Vector2(1, 0);
-                
+
             }
             else if (keyboard.IsKeyDown(Keys.A))
             {
@@ -72,21 +111,30 @@ namespace Sokoban
             }
             else if (keyboard.IsKeyDown(Keys.W))
             {
+
                 playerInput = new Vector2(0, -1);
+                trueAnimationsW = true;
+
             }
             else if (keyboard.IsKeyDown(Keys.S))
             {
                 playerInput = new Vector2(0, 1);
             }
 
-            placeTaken = LookAround.LookAt(GridPlacement.Placement(gridPosition + playerInput));
+
+            if (keyboard.IsKeyUp(Keys.W))
+            {
+                trueAnimationsW = false;
+            }
+
 
             //Movement
-            if (movementClock <= 0 && playerInput != Vector2.Zero && !placeTaken)
+            if (movementClock <= 0 && playerInput != Vector2.Zero)
             {
+
                 //Bounds
-                if(gridPosition.X + playerInput.X >= 0
-                    && gridPosition.X + playerInput.X < maxX 
+                if (gridPosition.X + playerInput.X >= 0
+                    && gridPosition.X + playerInput.X < maxX
                     && gridPosition.Y + playerInput.Y >= 0
                     && gridPosition.Y + playerInput.Y < maxY
                     )
@@ -96,16 +144,25 @@ namespace Sokoban
                     gridPosition += playerInput;
 
                     position = GridPlacement.Placement(gridPosition);
-                    rectangle.X = (int)position.X;
-                    rectangle.Y = (int)position.Y;
                 }
+
+
             }
+
         }
 
         public override void Update(GameTime gameTime)
         {
-            Animate(gameTime);
+            if (trueAnimationsW == true)
+            {
+                Animate(gameTime);
+                
+            }
+
+            AnimateD(gameTime);
             Movement(gameTime);
+
+            //Move(gameTime);
         }
 
         public override void OnCollision(GameObject other)
