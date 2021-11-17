afcaa8785bd5f0eca1c6ad609ef26068994dbf26
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2021-11-17 13:38:51 +0100

    fix

---
+++ MovementList.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sokoban
+{
+    class MovementList
+    {
+        private List<GameObjectWithMovement> objs = new List<GameObjectWithMovement>();
+        private List<Vector2> moves = new List<Vector2>();
+
+        public List<GameObjectWithMovement> Objs { get => objs; set => objs = value; }
+        public List<Vector2> Moves { get => moves; set => moves = value; }
+
+        public void InputMove(GameObjectWithMovement objectWithMovement, Vector2 lastPosition)
+        {
+            objs.Add(objectWithMovement);
+            moves.Add(lastPosition);
+        }
+    }
+}
